98e1caa238217b947f5eedc1bf7bb26b9928f669
cindyu1020 <cindyu1020@gmail.com>
2023-02-20 00:34:10 -0500

    Update image URLs and reps string

--- exercise_tile.py
+++ exercise_tile.py
@@ -3,10 +3,10 @@ from flask import abort
 
 EXERCISE_TILE = {
     "0" : {
-        "inlineIcon" : "https://cs210032000a530a4b5.blob.core.windows.net/lumblyimage/Physiotherapy.pdf?sv=2021-10-04&st=2023-02-20T04%3A15%3A54Z&se=2023-02-21T04%3A15%3A54Z&sr=b&sp=r&sig=KcxXy7KcIQPnf8%2FUCA%2BQL%2BZ6xd78U1f0xfY7nR%2BH7cw%3D",
+        "inlineIcon" : "https://cs210032000a530a4b5.blob.core.windows.net/lumblyimage/Physiotherapy.png?sv=2021-10-04&st=2023-02-20T05%3A11%3A41Z&se=2023-02-21T05%3A11%3A41Z&sr=b&sp=r&sig=JU9hfpsfpyvRcz2nBv0QN%2FRv6ItBzqOze61IwY80lls%3D",
         "exerciseName" : "Bird Dog",
-        "reps" : "10",
-        "exerciseImage" : "https://cs210032000a530a4b5.blob.core.windows.net/lumblyimage/Rectangle%208.pdf?sv=2021-10-04&st=2023-02-20T04%3A17%3A03Z&se=2023-02-21T04%3A17%3A03Z&sr=b&sp=r&sig=na2YfcN%2FlkuICpg8osimULXLUZPeum1O6IsFu%2BmazGk%3D"
+        "reps" : "10 Repetitions (per side)",
+        "exerciseImage" : "https://cs210032000a530a4b5.blob.core.windows.net/lumblyimage/Rectangle%208.png?sv=2021-10-04&st=2023-02-20T05%3A12%3A06Z&se=2023-02-21T05%3A12%3A06Z&sr=b&sp=r&sig=%2FzlHkdkS8dbdiP46jBFvx3KRasY8mOFuNmGVvuBBmfI%3D"
     }
 }
 
